67420e5d47c0d6f897562da1fa2bad8a577c3ccf
aleluman <alejandro.luman@protonmail.com>
2021-12-22 16:42:20 -0300

    :bug: Fixed pdf export

--- src/shared/utils/generatepdf.ts
+++ src/shared/utils/generatepdf.ts
@@ -1,26 +1,67 @@
-import { jsPDF as JsPDF } from "jspdf";
-import "jspdf-autotable";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { awareness, communication, transmission } from "../constants/services";
+
 import { queryClient } from "../state/store";
 import { AmbitType } from "../types/process";
 import { getAllInteractions } from "./allInteractions";
 
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const document: { content: any[]; styles: any } = {
+  content: [],
+  styles: {
+    header: {
+      fontSize: 18,
+      bold: true,
+      margin: [0, 0, 0, 10],
+    },
+    subheader: {
+      fontSize: 16,
+      bold: true,
+      margin: [0, 10, 0, 5],
+    },
+    tableExample: {
+      margin: [0, 5, 0, 15],
+    },
+    tableHeader: {
+      bold: true,
+      fontSize: 13,
+      color: "black",
+    },
+  },
+};
+
 export const generatePdf = (ambitId: string) => {
-  const doc = new JsPDF();
   const ambit = queryClient.getQueryData(["ambit", ambitId]) as AmbitType;
-  let visited = false;
+  document.content.push({ text: `Requirements for ambit ${ambit.name}`, style: "header" });
+
   ambit.graph.roles.forEach((role) => {
+    document.content.push({ text: `Interactions for role ${role.name}:`, style: "subheader" });
     const interactions = getAllInteractions(ambitId, role.id);
-    doc.text(`Interactions of ${role.name}`, 15, (doc as any).lastAutoTable.finalY + 15 || 15);
     interactions.forEach((inter) => {
-      const services = inter.services.map((service) => service.service).join(", ");
-      (doc as any).autoTable({
-        startY: (doc as any).lastAutoTable.finalY + (visited ? 5 : 20) || 20,
-        head: [[inter.roleName]],
-        body: [[services.length === 0 ? "No services" : services]],
+      const services = inter.services.map((service) => service.service);
+      const cServices = services.filter((service) => communication.includes(service)).join(", ");
+      const tServices = services.filter((service) => transmission.includes(service)).join(", ");
+      const aServices = services.filter((service) => awareness.includes(service)).join(", ");
+      document.content.push(`With ${inter.roleName}`);
+      document.content.push({
+        style: "tableExample",
+        table: {
+          unbreakable: true,
+          widths: ["*", "*", "*"],
+          body: [
+            ["Communication services", "Transmission services", "Awareness services"],
+            [
+              cServices.length > 0 ? cServices : { text: "No services", italics: true },
+              tServices.length > 0 ? tServices : { text: "No services", italics: true },
+              aServices.length > 0 ? aServices : { text: "No services", italics: true },
+            ],
+          ],
+        },
       });
-      visited = true;
     });
-    visited = false;
   });
-  doc.save("table.pdf");
+  pdfMake.createPdf(document).open();
 };
