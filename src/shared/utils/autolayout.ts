8edfad6cfdb540668474816430013379bcec507b
aleluman <alejandro.luman@protonmail.com>
2021-12-23 04:11:21 -0300

    :lipstick: Added better autolayout

--- src/shared/utils/autolayout.ts
+++ src/shared/utils/autolayout.ts
@@ -5,26 +5,29 @@ import { queryClient, useEditor } from "../state/store";
 import { AmbitType } from "../types/process";
 
 const physicsSettings = {
-  timeStep: 0.5,
+  timeStep: 2,
   dimensions: 2,
-  gravity: -12,
-  theta: 0.8,
-  springLength: 15,
+  gravity: -100,
+  theta: 1,
+  springLength: 8,
   springCoefficient: 0.8,
-  dragCoefficient: 0.9,
+  dragCoefficient: 0,
 };
 
 export const autoLayout = (ambitId: string): AmbitType["graph"] => {
   const graph = createGraph();
   const ambit = queryClient.getQueryData(["ambit", ambitId]) as AmbitType;
   const { roles, interactions } = ambit.graph;
+  const unlinkedRoles = roles.filter((role) => {
+    return interactions.every((inter) => inter.source !== role.id && inter.target !== role.id);
+  });
   roles.forEach((role) => graph.addNode(role.id));
   interactions.forEach((inter) => graph.addLink(inter.source, inter.target));
+  unlinkedRoles.forEach((role) => graph.addLink(role.id, interactions[0].source));
   const layout = createLayout(graph, physicsSettings);
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     layout.step();
   }
-
   const newRoles = roles.map((role) => {
     const position = layout.getNodePosition(role.id);
     return { ...role, x: position.x * 20, y: position.y * 20 };
